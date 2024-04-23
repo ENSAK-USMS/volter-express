108a68e4ccb0897275249f101e1eab964032b392
Yassir Loukilia <yassirloukilia2001@gmail.com>
2024-04-23 07:22:29 +0100

    Update RouteMap component with new waypoints, zoom level adjustment, and route calculation event handling

--- web-app/src/components/RouteMap.jsx
+++ web-app/src/components/RouteMap.jsx
@@ -8,36 +8,58 @@ L.Marker.prototype.options.icon = L.icon({
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"
 });
 
-export default function RouteMapModal({ waypoints }) {
+export default function RouteMapModal({ waypoints, onRouteCalculated }) {
   const [modalVisible, setModalVisible] = useState(false);
+  const [distance, setDistance] = useState("");
+  const [time, setTime] = useState("");
+  let map;
 
   useEffect(() => {
     const modal = document.getElementById("redtruck");
     const showModalListener = () => setModalVisible(true);
+    const hideModalListener = () => {
+      setModalVisible(false);
+      if (map) {
+        map.remove();
+        map = null;
+      }
+    };
 
     modal.addEventListener("shown.bs.modal", showModalListener);
+    modal.addEventListener("hidden.bs.modal", hideModalListener);
+
     return () => {
       modal.removeEventListener("shown.bs.modal", showModalListener);
+      modal.removeEventListener("hidden.bs.modal", hideModalListener);
     };
   }, []);
 
   useEffect(() => {
     if (modalVisible) {
-      const map = L.map("routingmap").setView([51.505, -0.09], 18); // Adjusted zoom level to 18
+      map = L.map("routingmap").setView([51.505, -0.09], 18); // Adjusted zoom level to 18
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; OpenStreetMap contributors"
       }).addTo(map);
 
-      L.Routing.control({
+      const control = L.Routing.control({
         waypoints: waypoints,
         routeWhileDragging: true,
         lineOptions: {
-            styles: [{ color: "blue", weight: 4 }] // Use the routeColor prop
+          styles: [{ color: "blue", weight: 4 }] // Use the routeColor prop
         }
       }).addTo(map);
+
+      control.on("routesfound", function (e) {
+        const routes = e.routes;
+        const route = routes[0]; // Assuming only one route
+        const { distance, time } = route.summary;
+        setDistance(distance);
+        setTime(time);
+        onRouteCalculated({ distance, time });
+      });
     }
-  }, [modalVisible, waypoints]);
+  }, [modalVisible, waypoints, onRouteCalculated]);
 
   return (
     <div>
